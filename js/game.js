f2345d31d8b5a9217507da70061bb998055d58e4
pandekar <pandeadhistanaya@gmail.com>
2024-10-11 07:41:24 +0700

    first init

---
+++ js/game.js
@@ -0,0 +1,7 @@
+export function getComputerChoice() {
+  // TODO: Implement computer choice logic
+}
+
+export function playRound(playerChoice, computerChoice) {
+  // TODO: Implement round logic
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+import { getComputerChoice, playRound } from './game.js';
+import { updateScore, updateMessage } from './ui.js';
+
+// TODO: Select DOM elements
+
+// TODO: Initialize game variables
+
+// TODO: Add event listeners to choice buttons
+
+// Main game logic
+function game(playerChoice) {
+  // TODO: Implement main game logic
+}
